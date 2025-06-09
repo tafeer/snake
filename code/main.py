28f67b8fd76b9581a3c86e371e0a6a21048eea25
tafer <tafeer.p@gmail.com>
2025-06-09 21:48:15 +0300

    first commit

---
+++ code/main.py
@@ -0,0 +1,26 @@
+from settings import *
+
+class Game:
+    def __init__(self):
+        #setup
+        pygame.init()
+        self.display_surface = pygame.display.set_mode(WINDOW_WIDTH, WINDOW_HEIGHT)
+        pygame.display.set_caption("Snake")
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        #groups
+        self.all_sprites = pygame.sprite.Group()
+        self.player_tiles = pygame.sprite.Groups()
+
+        #player
+        self.player = Player()
+
+    def run(self):
+        while self.running:
+            dt = self.clock.tick() / 1000
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.display_surface.fill("Black")
---
+++ code/settings.py
@@ -0,0 +1,6 @@
+import pygame
+from random import randint
+
+WINDOW_WIDTH = 700
+WINDOW_HEIGHT = 700
+BLOCK_SIZE = 50
